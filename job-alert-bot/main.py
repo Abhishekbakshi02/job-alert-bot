f704909dea62073551b1d198cf6a81c0db136f50
Abhishekbakshi02 <67265286+Abhishekbakshi02@users.noreply.github.com>
2026-07-30 20:50:22 +0530

    Update main.py

--- job-alert-bot/main.py
+++ job-alert-bot/main.py
@@ -11,7 +11,7 @@ from seen_jobs import load_seen, save_seen
 COMPANIES = [
     ("Greenhouse", "https://job-boards.greenhouse.io/greenhouse"),
     ("NewsBreak", "https://job-boards.greenhouse.io/newsbreak"),
-    ("QuantumLoopAI", "https://www.quantumloopai.com/careers/open-roles"),
+    ("QuantumLoopAI", "https://apply.workable.com/quantumloopai/?lng=en#jobs"),
 ]
 
 
